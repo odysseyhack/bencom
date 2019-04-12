7010512bed0f4b00f67b8d5d17fce063922b79f1
Gerard Nieborg <gerard@lookingforbooking.com>
2019-04-12 16:33:41 +0200

    Clean

--- BenChain.Api/BenChain.Api/SwaggerUI/custom.js
+++
@@ -1,5 +0,0 @@
-﻿document.title = 'BenChain.Api';
-var logo = document.getElementById('logo');
-logo.href = window.location.href;
-logo.firstChild.src = '/swaggerui/logo.png';
-logo.lastChild.textContent = 'BenChain API';
